da00c5e1a34cdfdb666e922a70d0eb6281d87c01
Fabian <fabian.moertter@gmx.net>
2022-09-22 18:10:40 +0200

    initial commit

--- lua/custom/chadrc.lua
+++ lua/custom/chadrc.lua
@@ -1,2 +1,14 @@
 local M = {}
+
+M.ui = {}
+
+M.plugins = {
+  ["neovim/lspconfig"] = {
+    config = function()
+      require "plugins.configs.lspconfig"
+      require "custom.plugins.lspconfig"
+    end,
+  },
+}
+
 return M
