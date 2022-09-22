60e9c5b9506a21af4c2de4c841eec6a13e6165c7
Fabian <fabian.moertter@gmx.net>
2022-09-22 19:49:50 +0200

    initial commit

--- lua/custom/chadrc.lua
+++ lua/custom/chadrc.lua
@@ -6,7 +6,38 @@ M.ui = {
 
 -- local userPlugins = require "custom.plugins" -- path to table
 
-M.plugins = require "custom.plugins"
+M.plugins = {
+  -- user = userPlugins,
+  -- install = userPlugins,
+
+  ["neovim/lspconfig"] = {
+    config = function()
+      require "plugins.configs.lspconfig"
+      require "custom.plugins.lspconfig"
+    end,
+  },
+  ["jose-elias-alvarez/null-ls.nvim"] = {
+    after = "nvim-lspconfig",
+    config = function()
+      require "custom.plugins.null-ls"
+    end,
+  },
+  -- run :MasonInstallAll
+  ["williamboman/mason.nvim"] = {
+    override_options = {
+      ensure_installed = {
+        "lua-language-server", -- lua
+        "stylua", -- lua
+        "pyright", -- python
+        "html-lsp", -- html
+        "css-lsp" -- css
+      },
+    },
+  },
+  ["kyazdani42/nvim-tree.lua"] = {},
+  ["tpope/vim-fugitive"] = {}, -- Git integration
+  ["tpope/vim-surround"] = {}, -- easy surround
+}
 
 M.mappings = require "custom.mappings"
 
