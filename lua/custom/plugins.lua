490009d3745a3cca3c4d908b7a87377ed06bffeb
Fabian <fabian.moertter@gmx.net>
2022-09-22 19:24:14 +0200

    initial commit

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -1,4 +1,35 @@
+
+
+
 return {
-  [ "tpope/vim-fugitive" ] = {}, -- Git integration
-  [ "tpope/vim-surround" ] = {}, -- easy surround
+  -- user = userPlugins,
+  -- install = userPlugins,
+
+  ["neovim/lspconfig"] = {
+    config = function()
+      require "plugins.configs.lspconfig"
+      require "custom.plugins.lspconfig"
+    end,
+  },
+  ["jose-elias-alvarez/null-ls.nvim"] = {
+    after = "nvim-lspconfig",
+    config = function()
+      require "custom.plugins.null-ls"
+    end,
+  },
+  -- run :MasonInstallAll
+  ["williamboman/mason.nvim"] = {
+    override_options = {
+      ensure_installed = {
+        "lua-language-server", -- lua
+        "stylua", -- lua
+        "pyright", -- python
+        "html-lsp", -- html
+        "css-lsp" -- css
+      },
+    },
+  },
+  ["kyazdani42/nvim-tree.lua"] = {},
+  ["tpope/vim-fugitive"] = {}, -- Git integration
+--   [ "tpope/vim-surround" ] = {}, -- easy surround
 }
